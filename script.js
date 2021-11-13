3ed3b9d05a4da4569f6529dc2a1b23e2440bf00f
Ramon Muñoz <ramonm2811@gmail.com>
2021-11-12 21:48:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+/* 
+const btnNewGame = document.querySelector('.btn--new');
+const btnRollDice = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current1 = document.querySelector('.current--0');
+const current2 = document.querySelector('.current--1');
+init();
+
+//Eventos
+btnRollDice.addEventListener('click', rollDice);
+
+//Funciones
+function init() {
+  const player1 = prompt('Nombre de player 1');
+  const player2 = prompt('Nombre de player 2');
+  document.querySelector('#name--0').textContent = player1;
+  document.querySelector('#name--1').textContent = player2;
+
+  document.getElementById('score--0').textContent = 0;
+  document.getElementById('score--1').textContent = 0;
+  document.querySelector('.dice').style.display = 'none';
+}
+
+function rollDice(player) {
+  const dice = Math.floor(Math.random() * 6) + 1;
+}
+ */
+
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
